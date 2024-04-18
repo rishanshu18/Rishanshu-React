86c5a419dfd86a064b790292340fbec10c1fd7e4
rishanshu <rishanshu.delwar@teamlease.com>
2024-04-18 20:45:18 +0530

    eps-2

--- App.js
+++ App.js
@@ -42,6 +42,8 @@
 //  //render
 
 //////////nested html example///////////
+import React from "react";
+import ReactDOM from "react-dom/client";
 
 const parent = React.createElement(
     "div",
@@ -49,15 +51,15 @@ const parent = React.createElement(
          React.createElement(
         "div",
         {id:"child"},[
-        React.createElement("h1",{},"I'm an h1 tag"),
-        React.createElement("h2",{},"I'm an h2 tag"),
+        React.createElement("h1",{},"This is Rishanshu "),
+        React.createElement("h2",{},"18-08-1999"),
         ]
         ),
         React.createElement(
             "div",
             {id:"child2"},[
-            React.createElement("h1",{},"I'm an h1 tag"),
-            React.createElement("h2",{},"I'm an h2 tag"),
+            React.createElement("h1",{},"Software Engineer"),
+            React.createElement("h2",{},"React JS"),
         ]
         )
     ]);
---
+++ README.md
@@ -0,0 +1,21 @@
+#Rishanshu-React
+
+
+#parcel
+
+-DEV Build
+-Local Server
+-automatically refreshing browser (HMR-- hot module replacement)
+-File watching algorithm - written in c++
+-Caching - faster Builds
+-Image Optimization
+-Minification 
+-Bundling
+-Compress
+-Consistent Hashing
+-Code Splitting
+-Differential Bundling - support older browser
+-error handling
+-https
+-tree shaking - remove unuse code
+different built for dev and production
